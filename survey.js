5e26d0374b89ad48b67decb389efc4e0c8e28c72
aldwiputra <aldiansyhdp@gmail.com>
2023-03-07 13:15:47 +0700

    add all questions

--- survey.js
+++ survey.js
@@ -1,15 +1,60 @@
 import inquirer from 'inquirer';
 
-function emptyStringValidationFactory(errorMsg) {
-  return async (input) => {
-    if (input.trim().length > 0) {
-      return true;
-    }
-    return errorMsg;
-  };
+function validationFactory(checker, errorMsg) {
+  return async (input) => (checker(input) ? true : errorMsg);
 }
 
-const questions = [];
+function emptyStringChecker(input) {
+  return input.trim().length > 0;
+}
+
+function validEmailChecker(input) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
+}
+
+const questions = [
+  {
+    type: 'input',
+    name: 'firstName',
+    message: "What's your first name?",
+    validate: validationFactory(emptyStringChecker, 'Name cannot be empty'),
+    filter: async (input) => input.trim().toLowerCase().charAt(0).toUpperCase() + input.slice(1),
+  },
+  {
+    type: 'input',
+    name: 'emailAddress',
+    message: (answers) => `Hello, ${answers.firstName}! What's your email address?`,
+    validate: validationFactory(validEmailChecker, 'Email has to be a valid email address'),
+  },
+  {
+    type: 'list',
+    name: 'isExperienced',
+    message: 'Are you an experienced developer?',
+    choices: ['yes', 'no'],
+  },
+  {
+    type: 'list',
+    name: 'yearsOfExperience',
+    message: 'How many years of experience you have with JavaScript?',
+    choices: ['0-1', '1-3', '3-5', '5-10', '10+'],
+    when: (answers) => (answers.isExperienced === 'yes' ? true : false),
+  },
+  {
+    type: 'checkbox',
+    name: 'jsLibraries',
+    message: 'How many javascript library do you know?',
+    choices: ['React.js', 'Vue', 'Angular', 'Node.js', 'jQuery', 'D3.js'],
+    when: (answers) => (answers.isExperienced === 'yes' ? true : false),
+    validate: async (input) => (input.length > 0 ? true : 'You have to choose at least one library'),
+  },
+  {
+    type: 'number',
+    name: 'expectedSalary',
+    message: 'What is your desired salary?',
+    when: (answers) => (answers.isExperienced === 'yes' ? true : false),
+    validate: async (input) => (input > 0 ? true : 'Salary cannot be 0'),
+  },
+];
 
 // run your command
 inquirer
