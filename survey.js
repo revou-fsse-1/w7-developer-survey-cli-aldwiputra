13935f1b6a6ebe0987da0d0bc181a845cc5a5543
aldwiputra <aldiansyhdp@gmail.com>
2023-03-08 09:30:02 +0700

    add script and fix filter fn for firstName

--- survey.js
+++ survey.js
@@ -1,7 +1,7 @@
 import inquirer from 'inquirer';
 
 function validationFactory(checker, errorMsg) {
-  return (input) => (checker(input) ? true : errorMsg);
+  return input => (checker(input) ? true : errorMsg);
 }
 
 function emptyStringChecker(input) {
@@ -18,12 +18,14 @@ const questions = [
     name: 'firstName',
     message: "What's your first name?",
     validate: validationFactory(emptyStringChecker, 'Name cannot be empty'),
-    filter: (input) => input.trim().toLowerCase().charAt(0).toUpperCase() + input.slice(1),
+    filter: input =>
+      input.replaceAll(' ', '').charAt(0).toUpperCase() +
+      input.replaceAll(' ', '').toLowerCase().slice(1),
   },
   {
     type: 'input',
     name: 'emailAddress',
-    message: (answers) => `Hello, ${answers.firstName}! What's your email address?`,
+    message: answers => `Hello, ${answers.firstName}! What's your email address?`,
     validate: validationFactory(validEmailChecker, 'Email has to be a valid email address'),
   },
   {
@@ -37,32 +39,32 @@ const questions = [
     name: 'yearsOfExperience',
     message: 'How many years of experience you have with JavaScript?',
     choices: ['0-1', '1-3', '3-5', '5-10', '10+'],
-    when: (answers) => (answers.isExperienced === 'yes' ? true : false),
+    when: answers => (answers.isExperienced === 'yes' ? true : false),
   },
   {
     type: 'checkbox',
     name: 'jsLibraries',
     message: 'How many javascript library do you know?',
     choices: ['React.js', 'Vue', 'Angular', 'Node.js', 'jQuery', 'D3.js'],
-    when: (answers) => (answers.isExperienced === 'yes' ? true : false),
-    validate: (input) => (input.length > 0 ? true : 'You have to choose at least one library'),
+    when: answers => (answers.isExperienced === 'yes' ? true : false),
+    validate: input => (input.length > 0 ? true : 'You have to choose at least one library'),
   },
   {
     type: 'number',
     name: 'expectedSalary',
     message: 'What is your desired salary?',
-    when: (answers) => (answers.isExperienced === 'yes' ? true : false),
-    validate: (input) => (input > 0 ? true : 'Salary cannot be 0'),
+    when: answers => (answers.isExperienced === 'yes' ? true : false),
+    validate: input => (input > 0 ? true : 'Salary cannot be 0'),
   },
 ];
 
 // run your command
 inquirer
   .prompt(questions)
-  .then((answers) => {
+  .then(answers => {
     console.log(JSON.stringify(answers, null, 2));
   })
-  .catch((error) => {
+  .catch(error => {
     if (error.isTtyError) {
       console.log('Your console environment is not supported!');
     } else {
